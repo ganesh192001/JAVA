4cc0c6a6ba67319a9d897e5f77326a95cb3836e4
Ganesh <ganeshjutla01@gmail.com>
2023-08-16 17:42:30 +0530

    Area of Circle

---
+++ Lecture1_10_08_23/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Circle C1 = new Circle();
+        C1.setX(20);
+        C1.setY(25);
+        C1.setR(5);
+        C1.displayValue();
+        C1.areaOfCircle();
+
+
+    }
+}
