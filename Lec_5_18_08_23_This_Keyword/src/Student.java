fc31f8cc81c709ac3c3f927d1e7d5a40f8ef6e96
Ganesh <ganeshjutla01@gmail.com>
2023-08-21 20:10:23 +0530

    Ignored unnecessary files

--- Lec_5_18_08_23_This_Keyword/src/Student.java
+++ Lec_5_18_08_23_This_Keyword/src/Student.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private int roll;
+    private String name;
+    private int marks;
+
+    public Student() {
+        roll = 111;
+        name = "Snehal";
+        marks = 90;
+    }
+
+    public Student(int roll, String name, int marks) {
+        this.roll = roll;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void display() {
+        System.out.println("Roll Number =" + " " +
+                roll + " " + "name =" + name + " " + "marks =" + marks);
+    }
 }
